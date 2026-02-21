d17d2d4d935458006cc3fc403b648df678099e74
Frank Harkins <frankharkins@hotmail.co.uk>
2026-02-21 23:32:45 +0000

    Improve accordions

--- src/lib/home/Accordion.svelte
+++ src/lib/home/Accordion.svelte
@@ -1,13 +1,28 @@
 <script>
 	import clsx from "clsx";
+	import { onMount } from "svelte";
 
     const { title, children } = $props();
+    const uid = $props.id();
 
     let expanded = $state(false);
+    let contentsHeightPx = $state(0);
+
+    function setContentsHeight() {
+      contentsHeightPx = document
+        .getElementById(uid)
+        ?.getBoundingClientRect()
+        .height ?? 2000;
+    }
 
     function toggleExpanded() {
       expanded = !expanded;
     }
+
+    onMount(() => {
+      window.addEventListener('resize', setContentsHeight);
+      setContentsHeight();
+    })
 </script>
 
 <button class="relative -m-px p-px hover:bg-title group block my-2 cursor-pointer" onclick={toggleExpanded}>
@@ -22,8 +37,13 @@
             <h2 class="font-bold">{title}</h2>
         </div>
 </button>
-<div class={clsx("py-0 transition-all duration-500 ease-out max-h-0", { "max-h-[1200px]": expanded })}>
-    {#if expanded}
+<div
+    class={clsx(
+        "py-0 transition-all duration-500 motion-reduce:duration-0 ease-out max-h-0 overflow-hidden",
+    )}
+    style={ expanded ? `max-height: ${contentsHeightPx}px` : "" }
+>
+    <div id={uid} class="box-border p-[0.5px]">
         {@render children()}
-    {/if}
+    </div>
 </div>
